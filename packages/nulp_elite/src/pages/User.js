2dbefad968425f5bedc66f38a3987df781e109b4
Mahesh Mahajan <mahajanmahesh935@gmail.com>
2024-03-12 20:56:07 +0530

    changed userService path

--- packages/nulp_elite/src/pages/User.js
+++ packages/nulp_elite/src/pages/User.js
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { Box, Heading, Text, Button } from "@chakra-ui/react";
 import URLSConfig from "../configs/urlConfig.json";
-import { userService } from "@shiksha/common-lib";
+import { userService } from "../Services/userService";
 
 const User = () => {
   const [data, setData] = useState({});
